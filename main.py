d875997f864c06a887fd12ab59c358f72ed30ea0
zeertag <taras0705@yandex.ru>
2023-01-04 23:03:42 +0300

    скелет

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Main branch")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def menu():
+    return render_template('menu.html')
+
+
+app.run(debug=True)
