19a6315dc0d58cbd862b41fb327ececff01925bc
zeertag <taras0705@yandex.ru>
2023-01-05 14:23:51 +0300

    убрал мусор

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def menu():
     return render_template('menu.html')
-print("frontend branch1")
 
-
-for i in range(1000):
-    pass
 
 app.run(debug=True)
-
-
